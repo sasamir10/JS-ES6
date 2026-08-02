91485d9567c38f202ace98eb0d2d5acae08be229
sasamir10 <sabbirahmed.samir18@gmail.com>
2026-08-02 09:29:36 +0600

    arrow function practise

---
+++ arrow.js
@@ -0,0 +1,13 @@
+// JS Arrow Function
+
+const greet = (name) => {
+    return `Hello ${name}`;
+};
+
+console.log(greet("Samir"));
+
+const sumNum = (a, b) => {
+    return a + b;
+};
+
+console.log(sumNum(70, 30));
