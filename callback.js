0da656f8440e028b1bc4d24ace74623ba82955d8
sasamir10 <sabbirahmed.samir18@gmail.com>
2026-08-07 11:15:23 +0600

    callback done

--- callback.js
+++ callback.js
@@ -1,8 +1,43 @@
-function greet() {
-    console.log("Heloo, whats up!");
-
-    return 8;
+/* function sum(a, b) {
+    return a + b;
 }
 
-console.log(greet());
-// greet();
+function num(n) {
+    return n;
+}
+
+const result = sum(80, num(10));
+
+console.log(result); */
+
+/* function calculate(a, callback) {
+    return callback(a);
+}
+
+function double(n) {
+    return n * 2;
+}
+
+const result = calculate(10, double);
+
+console.log(result); */
+
+function resgisterEntry(cb) {
+    console.log("User is registering");
+
+    return cb();
+}
+
+function userBasic() {
+    let student = {
+        name: "samir",
+        age: 25,
+    };
+
+    // console.log(student);
+    return student;
+}
+
+// console.log(resgisterEntry(userBasic));
+
+console.log(typeof undefined);
